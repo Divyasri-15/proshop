71e41a9760d5cd9dded0bf1476b501c4c3aefe03
Divya <divyasrivadlamudi@gmail.com>
2023-08-01 22:14:28 +0530

    Frontend Authentication

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,35 @@
 import React from "react";
-import { Badge, Navbar, Nav, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { Badge, Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
 import logo from "../assets/logo.png";
 
 const Header = () => {
     const { cartItems } = useSelector((state) => state.cart);
+    const { userInfo } = useSelector((state) => state.auth);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [logoutApiCall] = useLogoutMutation();
+
+    const logoutHandler = async () => {
+      try {
+        await logoutApiCall().unwrap();
+        dispatch(logout());
+        // NOTE: here we need to reset cart state for when a user logs out so the next
+        // user doesn't inherit the previous users cart and shipping
+        // dispatch(resetCart());
+        navigate('/login');
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -33,11 +56,25 @@ const Header = () => {
                 </Nav.Link>
               </LinkContainer>
 
-              <LinkContainer to="/login">
+              { userInfo ? (
+                <>
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </>
+              ) : (
+                <LinkContainer to="/login">
                 <Nav.Link>
                   <FaUser /> Sign In
                 </Nav.Link>
               </LinkContainer>
+              ) }
+
             </Nav>
           </Navbar.Collapse>
         </Container>
